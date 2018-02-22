4f3deafa835b7938a029b0f7d1caa51967bc5a66
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-22 18:03:37 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,11 @@ class PostsController < ApplicationController
 
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end
   end
 
   private
